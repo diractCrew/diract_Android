de3dabd20748ca05fbac021417d3fa73b0c42d18
baekcii <bsy8470@naver.com>
2026-02-14 18:57:50 +0900

    feat(di): firebase-messaging 의존성 주입

--- app/src/main/java/com/baek/diract/di/FirebaseModule.kt
+++ app/src/main/java/com/baek/diract/di/FirebaseModule.kt
@@ -2,6 +2,7 @@ package com.baek.diract.di
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
@@ -29,5 +30,11 @@ object FirebaseModule {
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseMessaging(): FirebaseMessaging {
+        return FirebaseMessaging.getInstance()
     }
 }
