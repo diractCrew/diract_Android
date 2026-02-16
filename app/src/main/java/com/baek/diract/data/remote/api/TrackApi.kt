25bf87d28fe4b3aa944c66ba9f3a81cfca225b6e
baekcii <bsy8470@naver.com>
2026-02-17 00:50:57 +0900

    feat: track api 구현 및 di 정의

---
+++ app/src/main/java/com/baek/diract/data/remote/api/TrackApi.kt
@@ -0,0 +1,53 @@
+package com.baek.diract.data.remote.api
+
+import com.baek.diract.data.remote.dto.ApiResponse
+import com.baek.diract.data.remote.dto.TrackDto
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface TrackApi {
+
+    //트랙(Track) 생성 API
+    @POST("api/tracks/{tracksId}/sections/{sectionId}/track")
+    suspend fun createTrack(
+        @Path("tracksId") tracksId: String,
+        @Path("sectionId") sectionId: String,
+        @Body request: CreateTrackRequest
+    ): ApiResponse<TrackDto>
+
+    //트랙(Track) 목록 조회 API
+    @GET("api/tracks/{tracksId}/sections/{sectionId}/track")
+    suspend fun getTrackList(
+        @Path("tracksId") tracksId: String,
+        @Path("sectionId") sectionId: String,
+    ): ApiResponse<TrackDto>
+
+    //트랙(Track) 수정 API
+    @PATCH("api/tracks/{tracksId}/sections/{sectionId}/track/{trackId}")
+    suspend fun editTrack(
+        @Path("tracksId") tracksId: String,
+        @Path("sectionId") sectionId: String,
+        @Path("trackId") trackId: String,
+        @Body request: EditTrackRequest
+    ): ApiResponse<TrackDto>
+
+    //트랙(Track) 삭제 API
+    @DELETE("api/tracks/{tracksId}/sections/{sectionId}/track/{trackId}")
+    suspend fun deleteTrack(
+        @Path("tracksId") tracksId: String,
+        @Path("sectionId") sectionId: String,
+        @Path("trackId") trackId: String
+    ): ApiResponse<Unit>
+}
+
+data class CreateTrackRequest(
+    val trackName: String
+)
+
+data class EditTrackRequest(
+    val trackName: String
+)
---
+++ app/src/main/java/com/baek/diract/data/remote/dto/TrackDto.kt
@@ -0,0 +1,10 @@
+package com.baek.diract.data.remote.dto
+
+data class TrackDto(
+    val trackId: String,
+    val trackName: String,
+    val tracksId: String,
+    val sectionId: String,
+    val createdAt: String,
+    val updatedAt: String
+)
