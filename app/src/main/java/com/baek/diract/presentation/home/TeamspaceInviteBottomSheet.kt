ed29bf9531102dc7a3e7c9e15394d620a56659be
User <bg7754@naver.com>
2026-02-19 18:29:54 +0900

    feat: 팀스페이스 생성완료 후 화면 구현

--- app/src/main/java/com/baek/diract/presentation/home/TeamspaceInviteBottomSheet.kt
+++ app/src/main/java/com/baek/diract/presentation/home/TeamspaceInviteBottomSheet.kt
@@ -9,7 +9,7 @@ import com.baek.diract.databinding.FragmentTeamspaceInviteBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
-
+import com.baek.diract.R
 
 @AndroidEntryPoint
 class TeamspaceInviteBottomSheet : BottomSheetDialogFragment() {
