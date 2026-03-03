9fb50288e7e63d1fa3e7a26213477979140deeed
baekcii <bsy8470@naver.com>
2026-03-04 00:28:26 +0900

    chore: 앱 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.baek.diract"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10001
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
